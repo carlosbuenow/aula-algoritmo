c88ba3def0014818df1d0b880f5e3e3c4ebf61b0
Carlos Eduardo Bueno de Souza <carlosbueno_53@hotmail.com>
2025-09-04 01:00:07 +0000

    Fix Ex4

--- lista-2/ex4.js
+++ lista-2/ex4.js
@@ -8,6 +8,12 @@ let resp1 = num1 + 5
 let resp2 = num2 + 5
 let resp3 = num3 + 5
 
-if(resp1>10, resp2>10, resp3>10){
-    console.log("O resultado é:", resp1, "O resultado do segundo número:", resp2, "O resultado do terceiro número:", resp3)
+if(resp1>10){
+console.log("O resultado 1° número é:", resp1)
+}   
+if(resp2>10){
+    console.log("O resultado do 2° número é:", resp2)
+} 
+if(resp3>10){
+    console.log("O resultado do 3° número é:", resp3)
 }
