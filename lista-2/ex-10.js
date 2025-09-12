a3eeeafb8f85ff8176f10ec3386d947154604f6e
Carlos Eduardo Bueno de Souza <carlosbueno_53@hotmail.com>
2025-09-12 14:10:56 -0300

    Fix Ex10

--- lista-2/ex-10.js
+++ lista-2/ex-10.js
@@ -2,16 +2,29 @@ import input from 'readline-sync'
 
 let Tabuada = input.questionInt("Digite um número:")
 
+Resp1 = Tabuada * 1
+Resp2 = Tabuada * 2
+Resp3 = Tabuada * 3
+Resp4 = Tabuada * 4
+Resp5 = Tabuada * 5
+Resp6 = Tabuada * 7
+Resp7 = Tabuada * 7
+Resp8 = Tabuada * 8
+Resp9 = Tabuada * 9
+Resp10 = Tabuada * 10
+
+
+
 if(Tabuada<10){
 console.log("-----------",Tabuada,"-----------");
-console.log('1 x', Tabuada, "=", Tabuada * 1);
-console.log('2 x', Tabuada, "=", Tabuada * 2);
-console.log('3 x', Tabuada, "=", Tabuada * 3);
-console.log('4 x', Tabuada, "=", Tabuada * 4);
-console.log('5 x', Tabuada, "=", Tabuada * 5);
-console.log('6 x', Tabuada, "=", Tabuada * 6);
-console.log('7 x', Tabuada, "=", Tabuada * 7);
-console.log('8 x', Tabuada, "=", Tabuada * 8);
-console.log('9 x', Tabuada, "=", Tabuada * 9);
-console.log('10 x', Tabuada, "=", Tabuada * 10);
+console.log('1 x', Tabuada, "=", resp1);
+console.log('2 x', Tabuada, "=", resp2);
+console.log('3 x', Tabuada, "=", resp3);
+console.log('4 x', Tabuada, "=", resp4);
+console.log('5 x', Tabuada, "=", resp5);
+console.log('6 x', Tabuada, "=", resp6);
+console.log('7 x', Tabuada, "=", resp7);
+console.log('8 x', Tabuada, "=", resp8);
+console.log('9 x', Tabuada, "=", resp9);
+console.log('10 x', Tabuada, "=", resp10);
 }
