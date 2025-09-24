1c02f542aced5e67b7222248aef92e0588613bbf
Carlos Eduardo Bueno de Souza <carlosbueno_53@hotmail.com>
2025-09-24 17:12:21 -0300

    Update ex10.js

--- lista-3/ex10.js
+++ lista-3/ex10.js
@@ -7,24 +7,22 @@ let mult1 = num1 * 10
 let mult2 = num2 * 10
 let div1 = num1 / 2
 let div2 = num2 / 2
-let soma1 = mult1 + div1
-let soma2 = mult2 + div2
-let soma3 = mult1 + div2
-let soma4 = mult2 + div1
+let soma1 = mult1 + div2
+let soma2 = mult2 + div1
 
 if(num1<num2){
-}if(soma3 % 2 == 0){
-    console.log("O resultado é par: ", soma3)
+}if(soma1 % 2 == 0){
+    console.log("O resultado é par: ", soma1)
     }else{
-        if(soma3 %2 == 1){
-        console.log("O resultado é ímpar: ", soma3)
+        if(soma1 %2 == 1){
+        console.log("O resultado é ímpar: ", soma1)
         }else{
             if(num2<num1){
-                if(soma4 %2 == 0){
-                console.log("O resultado é par: ", soma4)
+                if(soma2 %2 == 0){
+                console.log("O resultado é par: ", soma2)
                 }else{
-                    if(soma4 %2 == 1){
-                     console.log("O resultado é ímpar: ", soma4)}
+                    if(soma2 %2 == 1){
+                     console.log("O resultado é ímpar: ", soma2)}
             }
         }
     }
