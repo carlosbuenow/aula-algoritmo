d51137200a7f911d8f41a0ff0eac6b7950b6e68c
Carlos Eduardo Bueno de Souza <carlosbueno_53@hotmail.com>
2025-10-30 17:26:57 +0000

    Finishing

--- pa-assincrona/ex13.js
+++ pa-assincrona/ex13.js
@@ -1,3 +1,30 @@
 import input from 'readline-sync';
 
-let 
+let  numero = -1;
+let soma = 0;
+let cont = 0;
+let media = 0;
+let maior = null;
+let menor = null;
+
+while(numero != 0){
+    numero = input.questionInt("Digite um número (0 para sair): ")
+    if(numero != 0){
+        soma = soma + numero;
+        cont++;
+        if(menor == null && numero < menor){
+            menor = numero
+        }
+        if(maior == null && numero < maior){
+            maior = numero
+        }
+    }
+}
+
+if(cont > 0){
+    media = soma / cont;
+    console.log("Quantidade: ", cont);
+    console.log("Média: ", media)
+    console.log("Maior: ", maior)
+    console.log("Menor: ", menor)
+}
--- pa-assincrona/ex14.js
+++ pa-assincrona/ex14.js
@@ -1,1 +1,10 @@
 import input from 'readline-sync';
+
+let totalPagar = 0;
+let valor = -1;
+
+while(valor != 0){
+    valor = input.questionFloat("Digite um número (0 para sair): R$")
+    totalPagar = totalPagar + valor;
+}
+console.log("Total a pagar: ",totalPagar.toFixed(2));
