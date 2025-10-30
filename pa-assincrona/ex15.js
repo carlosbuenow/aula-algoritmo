3ed4d9737c29c3ab1ed96e501343aacb72d86991
Carlos Eduardo Bueno de Souza <carlosbueno_53@hotmail.com>
2025-10-30 17:54:01 +0000

    Finishing

--- pa-assincrona/ex15.js
+++ pa-assincrona/ex15.js
@@ -1,1 +1,31 @@
 import input from 'readline-sync';
+
+let opcao = -1;
+
+while(opcao != 0){
+    console.log("====== MENU PRINCIPAL ======");
+    console.log("1 - Contagem Simples (1 a 20)");
+    console.log("2 - Pares e Ímpares até um número");
+    console.log("3 - Soma de 5 Números");
+    console.log("4 - Contagem Regressiva");
+    console.log("5 - Tabuada");
+    console.log("6 - Média de Notas");
+    console.log("7 - Maior e Menor Número");
+    console.log("8 - Senha Correta");
+    console.log("9 - Contar Vogais");
+    console.log("10 - Fatorial");
+    console.log("11 - Sequencia ");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+    switch (key) {
+        case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
