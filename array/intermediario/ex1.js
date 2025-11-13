68be8d2fef2ca409143089d63142b3a44bae476d
Carlos Eduardo Bueno de Souza <carlosbueno_53@hotmail.com>
2025-11-13 01:28:48 +0000

    Working

---
+++ array/intermediario/ex1.js
@@ -0,0 +1,7 @@
+const numeros = [3,10,2,15,4,20,1,25]
+
+for(let i = 0; i < numeros.length; i++){
+    if(numeros[i] > 5){
+        console.log(numeros[i])
+    }
+}
---
+++ array/intermediario/ex3.js
@@ -0,0 +1,7 @@
+const cidades = ["Diadema","Belém", "Cotia", "Fortaleza","Franca","Lins"]
+
+for(let i = 0; i < cidades.length; i++){
+    if(cidades[i].charAt(5)){
+        console.log(cidades[i])
+    }
+}
